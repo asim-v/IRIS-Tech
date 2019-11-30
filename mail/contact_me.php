daa64648f12e980c792529dedc62e9f0dc5740a5
Turing <emoxic.sama@gmail.com>
2019-11-30 09:54:08 -0600

    Agregado contacto y acerca de

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce2c1ff972f2d626d1d7039e95df4253592aab96b9173c07ad3228b15a40ebf7
-size 1242
+oid sha256:e024418658507550b013e01eea315f73aeee15a43507f14b6f0baa48574d6387
+size 1241
