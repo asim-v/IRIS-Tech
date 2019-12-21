d3a700ddd659a5eed504b3e7fc0403e469ab017d
Turing <emoxic.sama@gmail.com>
2019-12-21 01:40:03 -0600

    Los SVG de Inkscape necesitan una view box que los haga reaccionar ante el mundo

--- js/waves.js
+++ js/waves.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d08e7788f736a64a42ab6e73ce9c93122da3b18a1077653ba333a7585883d652
-size 1435
+oid sha256:0dd3f5bfc81ef9be9c86337a0c4ff39fb037aa610bf1c8269475a54c9bd3c253
+size 1437
