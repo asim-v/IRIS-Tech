33fbe743a9a184cb523f3a5a00430c0c5abcf9ee
Turing <emoxic.sama@gmail.com>
2019-12-21 17:47:14 -0600

    Markup para agregar texto reparado, titulo a tono con el texto generado automaticamente

--- js/smartconvert.js
+++ js/smartconvert.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15a71468b83b1a7b9d181ac85220d4e37d6bcde5d7b6c0abe196c69c52793df2
-size 3098
+oid sha256:c07c21cf7bf2118a4628a16def15ba04558fea3b3695c788f799c05a8e31184f
+size 3339
