9edc5b748719cc61f82852d5296cc42fbdd0f161
Turing <emoxic.sama@gmail.com>
2019-12-21 17:22:46 -0600

    Integracion final de javascript

--- js/smartconvert.js
+++ js/smartconvert.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a98b1ba563c4283942aa7fa4335a86cb7f16436173cb2ce6d5cc3858aa9c67eb
-size 2850
+oid sha256:15a71468b83b1a7b9d181ac85220d4e37d6bcde5d7b6c0abe196c69c52793df2
+size 3098
