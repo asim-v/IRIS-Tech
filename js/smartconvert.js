1a8ed1a2429f533404ff3abf32020630b33c53cc
Turing <emoxic.sama@gmail.com>
2019-12-24 00:52:13 -0600

    Agregados hipervinculos a cada parte de la pagina, agregadas algunas imagenesde sponsors pues aun que se que no le gustaran se ven bien kul

--- js/smartconvert.js
+++ js/smartconvert.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c07c21cf7bf2118a4628a16def15ba04558fea3b3695c788f799c05a8e31184f
-size 3339
+oid sha256:8b2e0d597df91349f495cb818923633e48c429a5cd33f9c1603daaf55e6d84eb
+size 4478
