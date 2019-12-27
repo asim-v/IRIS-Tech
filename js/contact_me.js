7902fe2804b5b218104310704ccbff0e4b58c15b
Turing <emoxic.sama@gmail.com>
2019-12-26 20:56:55 -0600

    Agregado ventana inclinada responsiva, en balamcss Agregado mejor fondo con la imagen editada con mas saturacion y mejores colores, con botones morados

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22ef32d3f6c5b2b941308cfa949c54610dcfe4f45a2aab4677ab357f4c4d6fff
-size 2807
+oid sha256:da796be7462d4c762612f06c5d385caa3905e6500306616f7c400932fb4a18f3
+size 2805
--- js/smartconvert.js
+++ js/smartconvert.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b2e0d597df91349f495cb818923633e48c429a5cd33f9c1603daaf55e6d84eb
-size 4478
+oid sha256:9c666f2726add2eee33de89b302ce045224364539713ca96fdbee53c2dc369fa
+size 4384
